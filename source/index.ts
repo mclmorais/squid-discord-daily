2ce339859af4308660cf1c6244c207396931d0ff
Marcelo Morais <marcelodotmorais@gmail.com>
2021-12-05 18:18:47 -0300

    💚 added uncaught exception

--- source/index.ts
+++ source/index.ts
@@ -4,6 +4,12 @@ import { config as dotenvConfig } from 'dotenv'
 import { Bot } from './bot'
 import { PrismaClient } from '@prisma/client'
 
+process.on('uncaughtException', function (error)
+{
+  console.dir(error, { showHidden : false, depth : null, colors : true })
+  process.exit()
+})
+
 dotenvConfig()
 
 container.register<PrismaClient>(PrismaClient, { useValue : new PrismaClient() })
